e87dfef0ec7a852f765224a09f6a8ceac32a7859
JoseFarrelS <josefarrelsamgar_2226250006@mhs.mdp.ac.id>
2023-10-03 12:56:05 +0700

    Masukan Tugas 1.2

--- bin/dart_application_if3b.dart
+++ bin/dart_application_if3b.dart
@@ -1,6 +1,26 @@
 import 'package:dart_application_if3b/dart_application_if3b.dart'
     as dart_application_if3b;
+class PersegiPanjang {
+  PersegiPanjang(this.p, this.l);
+  int p = 0;
+  int l = 0;
 
-void main(List<String> arguments) {
-  print('Hello Everyone');
+  num hitungLuas() {
+    return p * l;
+  }
+
+  num hitungKeliling() {
+    return 2 * (p + l);
+  }
+}
+
+void main() {
+  int p = 8;
+  int l = 5;
+
+  num luas = PersegiPanjang(p, l).hitungLuas();
+  num keliling = PersegiPanjang(p, l).hitungKeliling();
+
+  print("Luas persegi panjang = $luas");
+  print("Keliling persegi panjang = $keliling");
 }
